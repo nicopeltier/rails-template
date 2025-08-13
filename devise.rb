c61df882806ad7d4db39b69fe3b525f255675276
Nicolas Peltier <nicolas.peltier@gmail.com>
2025-08-13 22:32:58 +0200

    final

--- devise.rb
+++ devise.rb
@@ -104,7 +104,7 @@ def setup_node_bundling
       bundle: true,
       outdir: path.join(process.cwd(), 'app/assets/builds'),
       absWorkingDir: path.join(process.cwd(), 'app/javascript'),
-      watch: process.argv.includes('--watch'),
+
       sourcemap: true,
       publicPath: '/assets',
     }).catch(() => process.exit(1));
