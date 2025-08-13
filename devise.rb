627e5afcb5efd3ec2a2524a6db35cef4b8c14716
Nicolas Peltier <nicolas.peltier@gmail.com>
2025-08-13 19:47:04 +0200

    intializers

--- devise.rb
+++ devise.rb
@@ -110,9 +110,7 @@ def setup_devise
             'config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"'
 
   # Configure Devise to use DELETE for sign-out, which is the secure default.
-  gsub_file "config/initializers/devise.rb",
-            /#\s*config\.sign_out_via = :delete/,
-            "  config.sign_out_via = :delete"
+  uncomment_lines "config/initializers/devise.rb", /config\.sign_out_via = :delete/
 
   generate "devise", "User" unless File.exist?("app/models/user.rb")
 
